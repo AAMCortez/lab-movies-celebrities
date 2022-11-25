f6a564aed3de1aeed340e85e42db704fbcf3e914
António Cortez <aamcortez@gmail.com>
2022-11-25 21:53:34 +0000

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,7 +4,7 @@ const Movies = require("../models/Movies.model");
 
 router.get("/movies/create", async (req, res, next) => {
    try {
-    const allCelebrities = await Celebrity.find();
+      const allCelebrities = await Celebrity.find();
       res.render("movies/new-movie");
    } catch (error) {
       next(error);
@@ -35,5 +35,60 @@ router.get("/movies", async (req, res, next) => {
       next(error);
    }
 });
+
+router.get("/movies/:id", async (req, res, next) => {
+   try {
+      const { id } = req.params;
+      const movies = await Movies.findById(id).populate("cast");
+      const celebrities = await Celebrity.find();
+      const { _id, title, genre, plot, cast } = movies;
+      res.render("movies/movie-details", {
+         _id,
+         title,
+         genre,
+         plot,
+         cast,
+      });
+   } catch (error) {
+      next(error);
+   }
+});
+
+router.post("/movies/:id/delete", async (req, res, next) => {
+   try {
+      const { id } = req.params;
+      await Movies.findByIdAndDelete(id);
+      res.redirect("/movies");
+   } catch (error) {
+      next(error);
+   }
+});
+
+router.get("movies/:id/edit", async (req, res, next) => {
+   try {
+      const { id } = req.params;
+      await Movies.findById(id);
+      await Celebrity.find();
+      res.render("/movies/edit-movie");
+   } catch (error) {
+      next(error);
+   }
+});
+
+router.post("/movies/:id/edit", async (req, res, next) => {
+    try {
+       const { id } = req.params;
+       const { _id, title, genre, plot, cast } = req.body;
+       const updatedMovie = await Movies.findByIdAndUpdate(id, {
+        title,
+        genre,
+        plot,
+        cast
+       });
+       res.redirect(`/movies/${updatedMovie}`);
+    } catch (error) {
+       next(error);
+    }
+ });
 
 module.exports = router;
